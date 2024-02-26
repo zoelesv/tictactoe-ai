5964da509531566cedb31fe70c57d5887fd6c5f5
zoelesv <zoomlee1141@gmail.com>
2024-02-25 17:30:12 -0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# tictactoe-ai
+# Tic-tac-toe
+| ![Image 1](/src/play.png) | ![Image 2](/src/ai.png) |
+|:---------------------:|:---------------------:|
+## About
+This app implements an AI to play Tic-Tac-Toe optimally with an user.
+
+## Files
+- tictactoe.py - all of the logic for playing the game, and for making optimal moves
+- runner.py - pygame graphical interface
+
+## Instruction
+```pip install -r requirements.txt```
+```python runner.py```
+
+## Specifications:
+- S₀: Initial state (in our case, an empty 3X3 board)
+- Players(s): a function that, given a state s, returns which player’s turn it is (X or O).
+- Actions(s): a function that, given a state s, return all the legal moves in this state (what spots are free on the board).
+- Result(s, a): a function that, given a state s and action a, returns a new state. This is the board that resulted from performing the action a on state s (making a move in the game).
+- Terminal(s): a function that, given a state s, checks whether this is the last step in the game, i.e. if someone won or there is a tie. Returns True if the game has ended, False otherwise.
+- Utility(s): a function that, given a terminal state s, returns the utility value of the state: -1, 0, or 1.
+- Minimax function: using minimax algorith to search for the best optimal moves
+    ![Image 4](/src/state.png)
+    - Minimax represents winning conditions as (-1) for one side and (+1) for the other side
+    ![Image 3](/src/minimax.png)
+    - Given a state s, the function considers all the possible values of Future States
+        - MAX picks action a in ACTIONS(s) that produces highest value of MIN-VALUE(RESULT(s, a))
+        - MIN picks action a in ACTIONS(s) that produces smallest value of MAX-VALUE(RESULT(s, a))
---
+++ tictactoe.py
@@ -0,0 +1,146 @@
+"""
+Tic Tac Toe Player
+"""
+import math
+import copy
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    if board == initial_state():
+        return X
+    rows = board[0]+board[1]+board[2]
+    count_x = rows.count("X")
+    count_o = rows.count("O")
+    if count_x > count_o:
+        return O
+    else:
+        return X
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    result = set()
+    for i in range(3):
+        for j, value in enumerate(board[i]):
+            if value == EMPTY:
+                result.add((i, j))
+    return result
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    new_board = copy.deepcopy(board)
+    try:
+        new_board[action[0]][action[1]] = player(new_board)
+        return new_board
+    except IndexError:
+        print("Invalid spot")
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    for row in board:
+        count_x = row.count(X)
+        count_o = row.count(O)
+        if count_x == 3:
+            return X
+        elif count_o == 3:
+            return O
+    for i in range(3):
+        if board[0][i] == board[1][i] == board[2][i] == X:
+            return X
+        elif board[0][i] == board[1][i] == board[2][i] == O:
+            return O
+    if board[0][0] == board[1][1] == board[2][2] == X:
+        return X
+    elif board[0][0] == board[1][1] == board[2][2] == O:
+        return O
+    elif board[0][2] == board[1][1] == board[2][0] == X:
+        return X
+    elif board[0][2] == board[1][1] == board[2][0] == O:
+        return O
+    return None
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    count_empty = (board[0]+board[1]+board[2]).count(EMPTY)
+    return count_empty == 0 or winner(board) is not None
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    if terminal(board):
+        return None
+    current_player = player(board)
+    if current_player == X:
+        v = -math.inf
+        for action in actions(board):
+            k = min_value(result(board, action))
+            if k > v:
+                v = k
+                best_move = action
+    else:
+        v = math.inf
+        for action in actions(board):
+            k = max_value(result(board, action))
+            if k < v:
+                v = k
+                best_move = action
+    return best_move
+
+
+def max_value(board):
+    if terminal(board):
+        return utility(board)
+    v = - math.inf
+    for action in actions(board):
+        v = max(v, min_value(result(board, action)))
+    return v
+
+
+def min_value(board):
+    if terminal(board):
+        return utility(board)
+    v = math.inf
+    for action in actions(board):
+        v = min(v, max_value(result(board, action)))
+    return v
